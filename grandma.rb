e68021df0fe200617213842864a567817bb24844
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-15 01:35:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 
 
 
-speak_to_grandma()
+speak_to_grandma("I LOVE YOU TOO PUMPKIN!")
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
