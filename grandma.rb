67a348fad73a65c3ca2a84b9e0f1ecd1a23f6df7
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-15 01:43:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,18 +1,14 @@
+ 
  def speak_to_grandma(string)
    if string == "I LOVE YOU GRANDMA!"
- putS "I LOVE YOU TOO PUMKIN!"
- elsif  string == string.upcase
- puts "NO, NOT SINCE 1938!"
- else
-   return "HUH?! SPEAK UP, SONNY!"
- end
+     return "I LOVE YOU TOO PUMKIN!"
+     elsif string == string.upcase
+     return "NO, NOT SINCE 1938"
+   else
+     return "HUH?! SPEAK UP, SONNY!"
+   end
  end
  
-   
-
-
-
-speak_to_grandma("I LOVE YOU TOO PUMPKIN!")
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
