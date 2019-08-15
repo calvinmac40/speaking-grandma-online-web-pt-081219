f17f2d906de1e633e19626011b0e21ac99f98744
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-15 01:38:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
  def speak_to_grandma(string)
-   if #{string}.upcase
- puts "HUH?! SPEAK UP, SONNY!"
- elsif #{string}.upcase == #{string}
+   if string == "I LOVE YOU GRANDMA!"
+ putS "I LOVE YOU TOO PUMKIN!"
+ elsif  string == string.upcase
  puts "NO, NOT SINCE 1938!"
  else
    return "HUH?! SPEAK UP, SONNY!"
